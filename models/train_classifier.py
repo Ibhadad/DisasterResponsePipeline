9ef868c55e2b95998b97fb4ab289054f99448c0e
Ibrahim El Haddad <ibhadad@gmail.com>
2019-12-03 19:38:14 +0200

    Add files via upload
    Checking the methods and make sure all of them is running without errors.

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,3 +1,4 @@
+#%matplotlib inline
 import sys
 # import libraries
 import pandas as pd
@@ -19,17 +20,17 @@ from sklearn.multioutput import MultiOutputClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics import f1_score
 from sklearn.model_selection import GridSearchCV
-%matplotlib inline
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report, accuracy_score, precision_score, recall_score, f1_score
 url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 
 def load_data(database_filepath):
-    engine = create_engine('sqlite://{}'.format(database_filepath))
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
     df = pd.read_sql_table('Messages', con=engine)
     X = df['message']    
-    Y = df[df.columns[4:]]
-    YY = df.columns[4:]
+    Y = df[df.columns[4:]].values
+    YY = df.columns[4:]    # categories names
+    return X, Y, YY
 
 
 def tokenize(text):
@@ -50,27 +51,36 @@ def tokenize(text):
 
 def build_model():
     mlc_1 = MultiOutputClassifier(RandomForestClassifier())
-    X_train, X_test, y_train, y_test = train_test_split(X, Y)
+   # X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.1)
     pipeline = Pipeline([
            ('vect', CountVectorizer(tokenizer=tokenize)),
            ('tfidf', TfidfTransformer()),
            ('clf', mlc_1)
            #('clf', RandomForestClassifier())
             ])
-    pipeline.fit(X_train, y_train)
+    #pipeline.fit(X_train, y_train)
    
     parameters = {'clf__estimator__n_estimators':[10, 25]
                   }
     cv = GridSearchCV(pipeline, parameters)
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-     y_pred = pipeline.predict(X_test)
+     y_pred = model.predict(X_test)
+     for i in range(0, len(category_names)):
+        print(category_names[i])
+        print("\tAccuracy: {:.4f}\t\t% Precision: {:.4f}\t\t% Recall: {:.4f}\t\t% F1_score: {:.4f}".format(
+            accuracy_score(Y_test[:, i], y_pred[:, i]),
+            precision_score(Y_test[:, i], y_pred[:, i], average='weighted'),
+            recall_score(Y_test[:, i], y_pred[:, i], average='weighted'),
+            f1_score(Y_test[:, i], y_pred[:, i], average='weighted')
+        ))
 
 
 def save_model(model, model_filepath):
     with open(model_filepath, 'wb') as file:
-    pickle.dump(model, file)
+        pickle.dump(model, file)
 
 
 def main():
@@ -78,7 +88,7 @@ def main():
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1)
         
         print('Building model...')
         model = build_model()
